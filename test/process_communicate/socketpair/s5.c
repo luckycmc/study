07bf1b69ac28a6c17c4dcf903babb77a576705f4
admin123456 <1765321014@qq.com>
2021-08-23 22:18:20 +0800

    线程通过匿名管道通讯

--- test/process_communicate/socketpair/s5.c
+++ test/process_communicate/socketpair/s5.c
@@ -14,11 +14,61 @@ struct pipe_rw
 //线程函数
 void *thread_handle(void *)
 {
-
+    //实例化指针结构体
+    struct pipe_rw *pPipeRw = (struct pipe_rw *)arg;
+    char buf[512];
+    int len;
+    //循环读写数据
+    while(1)
+    {
+          //线程从管道中读取数据
+          len = read(pPipeRw->fd_r,buf,512);
+          buf[len] = '\0';
+          printf("%s\n",buf);
+          //格式化数据处理
+          len = sprintf(buffer,"hello my father");
+          buf[len] = '\0';
+          //写入写管道
+          write(pPipeRw->fd_w,buf,len);
+          sleep(3);
+    }
 }
 //主函数
 int main()
 {
+    int fd[2];   //fd[0]是读，fd[1]是写
+    int fd2[2];
+    int res = pipe(fd);
+    if (res) {
+       printf("create pipe error\n");
+       return -1;
+    }
+    res = pipe(fd2);
+    if (res) {
+       printf("create pipe2 error\n");
+       return -1;
+    }
+    pthread_t thread; //线程id 变量
 
+    struct pipe_rw pipe_arg;  //创建结构体
+
+    pipe_arg.fd_r = fd[0];   //读管道
+    pipe_arg.fd_w = fd2[1];  //写管道
+    //创建对应的线程
+    pthread_create(&thread, NULL, thread_handle, &pipe_arg);
+    char buf[512];
+    int len;
+    while (1) {
+        len = sprintf(buf, "hello my child");
+        buf[len] = '\0';
+        //写数据
+        write(fd[1], buf, len);
+        //读数据
+        len = read(fd2[0], buf, 512);
+        buf[len] = '\0';
+        printf("%s\n", buf);
+        sleep(3);
+    }
+    return 0;
 }
 // 参考 https://www.cnblogs.com/zhu-g5may/p/10555238.html
