32db2a12328333ae516c688cfeb1cd2f4e4111a1
zpw1765321014 <1765321014@qq.com>
2021-09-10 10:17:53 +0800

    2021-09-10 线程读写锁

--- test/pthread/p18.c
+++ test/pthread/p18.c
@@ -19,4 +19,61 @@ int counter;
 pthread_rwlock_t rwlock;
 
 //写线程操作空间
+void *th_write(void *arg)
+{
+    int t;
+    int i = *(int *)arg;
+    while(1)
+    {
+         t = counter; // 保存写之前的值
+         usleep(1000);
+         //加读锁
+         pthread_rwlock_wrlock(&rwlock);
+         printf("=======write %d: %lu: counter=%d ++counter=%d\n", i, pthread_self(), t, ++counter);
+         //解锁
+         pthread_rwlock_unlock(&rwlock);
+         usleep(9000);   //给读锁提供读取的机会
+    }
+    return NULL;
+}
 // 读线程操作空间
+void *th_read(void *arg)
+{
+    int i = *(int *)arg;
+    while (1)
+    {
+            pthread_rwlock_rdlock(&rwlock);
+            printf("----------------------------read %d: %lu: %d\n", i, pthread_self(), counter);
+            pthread_rwlock_unlock(&rwlock);
+            usleep(2000);   //给写锁提供机会
+    }
+    return NULL; //返回地址空间
+}
+//主函数
+int main()
+{
+     int i;
+     pthread_t tid[8];
+     //初始化读写锁
+     pthread_rwlock_init(&rwlock, NULL);
+     
+     //创建写线程
+     for ( i = 0; i < 3; i++)
+     {
+          pthread_create(&tid[i], NULL,th_write,(void *)&i);  //&i取出对应的地址
+     }
+     //创建读线程
+     for ( i = 0; i <5; ++i)
+     {
+         pthread_create(&tid[i+3], NULL,th_read,(void *)&i); //&i取出对应的地址
+     }
+
+     //回收子线程
+     for ( i = 0; i <8;i++)
+     {
+         pthread_join(tid[i], NULL);
+     }
+     //释放读写锁
+     pthread_rwlock_destroy(&rwlock);
+     return 0; //
+}
