f5901f235bb92a4e9be4b34b2a751d8996b37b93
admin123456 <1765321014@qq.com>
2021-10-25 22:57:06 +0800

    go-imserver 用户模块封装

--- go语言学习/go_IM_System/server.go
+++ go语言学习/go_IM_System/server.go
@@ -2,11 +2,18 @@ package main
 import(
 	"fmt"
 	"net"
+	"sync"
+	"io"
 )
 //server struct
 type Server struct{
 	Ip string
 	Port int
+	//在线用户列表
+	OnlineMap map[string]*User  //在线用户有多少
+	mapLock sync.RWMutex
+	//消息广播的channel
+	Message chan string     //广播用户的消息
 }
 
 //创建一个server的接口
@@ -15,14 +22,71 @@ func NewServer(ip string,port int) *Server{
 	 server := &Server{
 		   Ip:ip,
 		   Port:port,
+		   OnlineMap : make(map[string]*User), 
+		   Message : make(chan string)
 	 }
 	 return server
 }
+//监听message 广播所有的 协成 一旦有消息就发送给全部的在线User
+func (this *Server) ListenMessage()  {
+	  
+	   for{
+		   msg := <- this.Message
+
+		   //把消息发送给所有的在线用户
+		   this.mapLock.Lock()
+		   for _,cli := range this.OnlineMap{
+			   cli.C <- msg
+		   }
+		   this.mapLock.Unlock()
+	   }
+}
+//广播消息
+func (this *Server) BroadCast(user *User,msg string)  {
+	  
+	  sendMsg := "[" + user.Addr+"]" +user.Name+":"+msg
+	  this.Message <- sendMsg
+}
+ 
 //处理对应的业务
 func (this *Server) Handler(conn net.Conn){
      
-	 defer conn.Close(); //close
-	 fmt.Println("连接建立成功")
+	 //defer conn.Close(); //close
+	 //fmt.Println("连接建立成功")
+	 user := NewUser(conn,this)
+	 //用户上线 将用户加入到online中
+     user.Online()
+	 
+	 //接受客户端消息
+	 go func ()  {
+		   
+		    buf := make([]byte, 4096)
+			//读取用户发送的信息
+			for{
+                
+				  n,err := conn.Read(buf)
+				  //客户端关闭
+				  if n==0 {
+					   
+					    user.Offline()
+						return
+				  }
+				  //数据读取错误
+				  if err != nil && err != io.EOF {
+					  
+					  fmt.Println("Conn Read err:",err)
+					  return
+				  }
+				  //提取用户消息（去除\n）
+				  msg := string(buf[:n-1])
+
+				  //用户针对message 
+				  user.DoMessage(msg)
+			}
+	 }()
+
+	 //当前handle 阻塞
+	 select{}
 }
 //启动服务的接口
 func (this *Server) Start() {
@@ -37,6 +101,9 @@ func (this *Server) Start() {
 	 }
 	 //关闭监听
 	 defer listener.Close()
+	 //启动监听Message 的goroutine
+	 go this.ListenMessage()
+
 	 //接受客户端请求
 	 for{
 
--- go语言学习/go_IM_System/user.go
+++ go语言学习/go_IM_System/user.go
@@ -8,10 +8,11 @@ type User struct{
 	Addr string
 	C    chan string
 	conn net.Conn
+	server *Server  //当前用户的server
 }
 
 //创建一个用户API
-func NewUser(conn net.Conn) *User {
+func NewUser(conn net.Conn,server *Server) *User {
 	
 	   userAddr := conn.RemoteAddr().String()  //转换成字符串
 	   
@@ -21,12 +22,37 @@ func NewUser(conn net.Conn) *User {
 			Addr: userAddr,
 			C:    make(chan string),
 			conn :conn,
+			server : server,
 	   }
        //启动监听当前User channel 消息的gorontine
 	   go user.ListenMessage()
+
 	   return user
 }
 
+//用户的上线业务
+func (this *User) Online() {
+	  //用户上线 将用户加入到online中
+	  this.server.mapLock.Lock()
+	  this.server.OnlineMap[this.Name] = this
+	  this.server.mapLock.Unlock()
+	  //广播当前用户消息
+	  this.server.BroadCast(this,"已上线")
+
+}
+//用户的下线业务
+func (this *User) Offline() {
+	  //用户上线 将用户加入到online中
+	  this.server.mapLock.Lock()
+	  delete(this.server.OnlineMap,this.Name)
+	  this.server.mapLock.Unlock()
+	  //广播当前用户消息
+	  this.server.BroadCast(this,"已下线")
+}
+//用户处理消息的业务
+func (this *User) DoMessage(msg string)  {
+	this.server.BroadCast(this,msg)
+}
 //监听当前User channnel 的方法，一旦有消息，直接发送给对端客户
 func (this *User) ListenMessage() {
 	  
