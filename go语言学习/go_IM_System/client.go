25d390d9778e81dec04bc2626887e1a7daa26418
zpw1765321014 <1765321014@qq.com>
2021-10-28 20:09:33 +0800

    2021-10-28 go_im_server 私聊模式

--- go语言学习/go_IM_System/client.go
+++ go语言学习/go_IM_System/client.go
@@ -3,6 +3,8 @@ import (
 	"net"
 	"fmt"
 	"flag"
+	"io"
+	"os"
 )
 //客户端结构体
 type Client struct{
@@ -33,6 +35,12 @@ func NewClient(serverIp string, serverPort int) *Client {
 	  //返回对象
 	  return client
 }
+//处理server回应的消息 直接显示到标准输出即可
+func (client *Client) DealResponse(){
+	  
+	  //一旦client.conn有数据 就直接copy到stdout标准输出上，永久阻塞监听
+	  io.Copy(os.Stdout, client.conn)
+}
 //菜单
 func (client *Client) menu() bool { 
      
@@ -53,6 +61,45 @@ func (client *Client) menu() bool {
 		   return false
 	   }
 }
+//公聊模式
+func (client *Client) PublicChat()  {
+	  
+	   //提示用户输入消息
+	   var chatMsg string
+	   fmt.Println(">>>>请输入聊天内容,exit退出")
+       fmt.Scanln("%s", &chatMsg)
+	   //只要不退出就接受用户消息
+	   for chatMsg != "exit" {
+              
+		      //发送消息
+			  if len(chatMsg) !=0 {
+				  
+				   sendMsg := chatMsg+"\n"
+				   _,err := client.conn.Write([]byte(sendMsg))
+				   if err != nil {
+					   fmt.Println("conn Write err:",err)
+					   break
+				   }
+			  }
+			  
+			  chatMsg =""  //消息重置为空
+			  fmt.Println(">>>>请输入聊天内容,exit退出")
+              fmt.Scanln(&chatMsg)
+	   }
+}
+//更新用户名
+func (client *Client) UpdateName() bool {
+	  
+	   fmt.Println(">>>>>请输入用户名:")
+	   fmt.Scanln(&client.Name)
+	   sendMsg := "rename|"+client.Name+"\n"
+	   _,err := client.conn.Write([]byte(sendMsg))
+	   if err != nil {
+		    fmt.Println("conn.Write err:",err)
+			return false
+	   }
+	   return true
+}
 //主业务
 func (client *Client) run(){
      
@@ -65,15 +112,16 @@ func (client *Client) run(){
 		   switch client.flag{
 		        case 1:
 					//公聊模式
-					fmt.Println("公聊模式选择...")
+					client.PublicChat()
 					break
 			    case 2:
 					fmt.Println("私聊模式选择...")
 					//私聊模式
 					break
 				case 3:
-					fmt.Println("更新用户名选择")
+					//fmt.Println("更新用户名选择")
 					//修改用户名
+					client.UpdateName()
 					break
 		   }
 	  }
@@ -99,6 +147,9 @@ func main() {
 		   fmt.Println(">>>>>>服务器连接失败....")
 		   return
 	   }
+	   //单独开启一个goroutine 去处理server的回执消息
+	   go client.DealResponse()
+
 	   fmt.Println(">>>>>>服务器连接成功......")
 
 	   //启动客户端业务
