1a1877dc1dfc1b2493360319ecf1038054fbb3dc
admin123456 <1765321014@qq.com>
2021-10-29 22:52:25 +0800

    tcp发送开发

--- go语言学习/data structure/tcp_send.go
+++ go语言学习/data structure/tcp_send.go
@@ -1,13 +1,38 @@
 package main
+import (
+	"fmt"
+	"net"
+)
 
 type Client struct {
      
 	    status string   //tcp的状态
 		window int      //tcp的窗口
+		conn net.Conn
+		data string
+}
+
+func createClient()  *Client{
+	 
+	  client := &Client{
+		  status:"send",
+		  window :64,
+		  data:"syn"
+	  }
 }
 //三握手
+func (this *Client) hand() bool{
+	
+}
 //发送数据
+func (this *Client)  sendData() int {
+	
+}
 //四次挥手
+func (this *Client) byeHand() bool  {
+	
+}
+//主函数
 func main()  {
 	
 }
