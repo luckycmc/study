5f88f3776ad2a33fef354b2abdb73fe315b0c2c2
admin123456 <1765321014@qq.com>
2021-11-25 21:48:26 +0800

    2021-11-25 队列

---
+++ go语言学习/data structure/queue/singleQueue.go
@@ -0,0 +1,15 @@
+package main
+import(
+	"fmt"
+)
+//使用一个结构体管理队列
+type Queue struct{
+	maxSize 4
+	array [4]int   //数组=>模拟队列
+	front int      //表示指向队首
+	rear  int      // 表示指向队列尾部
+}
+//主函数
+func main()  {
+	
+}
