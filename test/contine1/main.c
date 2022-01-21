2ecd777833e6ac7247ec1fa3915c7af779e8d9dc
zpw1765321014 <1765321014@qq.com>
2022-01-21 15:16:28 +0800

    continue协成

---
+++ test/contine1/main.c
@@ -0,0 +1,43 @@
+int stop = 0;
+static void foo_5(schedule_t *S, void *ud) {
+    while (!stop) {
+        co_yield(S);
+    }
+}
+
+static void test5(schedule_t *S) {
+    printf("test5 start===============\n");
+    struct timeval begin;
+    struct timeval end;
+    int i;
+    int count = 10000;
+
+    gettimeofday(&begin, NULL);
+    for (i = 0; i < count; ++i) {
+        co_new(S, foo_5, NULL);
+    }
+    gettimeofday(&end, NULL);
+    printf("create time=%f\n", timediff(&begin, &end));
+
+    gettimeofday(&begin, NULL);
+    for (i =0; i < 1000000; ++i) {
+        int co = (i % count) + 1;
+        co_resume(S, co);
+    }
+    gettimeofday(&end, NULL);
+    printf("swap time=%f\n", timediff(&begin, &end));
+
+    // 先释放掉原来的
+    stop = 1;
+    for (i = 0; i < count; ++i) {
+        int co = (i % count) + 1;
+        co_resume(S, co);
+    }
+    gettimeofday(&begin, NULL);
+    for (i = 0; i < count; ++i) {
+        co_new(S, foo_5, NULL);
+    }
+    gettimeofday(&end, NULL);
+    printf("create time2=%f\n", timediff(&begin, &end));
+    printf("test5 end===============\n");
+}
