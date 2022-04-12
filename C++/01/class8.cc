6466eaa91d728a859d6aa506075b85b0167b8b7d
admin123456 <1765321014@qq.com>
2022-04-12 22:51:02 +0800

    C++成员和方法

--- C++/01/class8.cc
+++ C++/01/class8.cc
@@ -1,2 +1,47 @@
 #include <iostream>
 using namespace std;
+class Box
+{
+	public:
+		double  length;         //�� 
+		double  breadth;        // ����
+		double   height;         // �߶�
+		//������Ա����
+		double getVolue(void);
+		void setLength( double len );
+        void setBreadth( double bre );
+        void setHeighth( double hei ); 
+}; 
+
+//��Ա�����Ķ���
+
+double Box::getVolue(void)
+{
+	return length * breadth *height;
+} 
+
+void Box::setLength(double len)
+{
+	length = len;
+}
+void Box::setBreadth( double bre )
+{
+	breadth = bre;
+}
+void Box::setHeighth(double hei)
+{
+	height = hei;
+}
+
+
+int main()
+{
+	Box b1;
+	double volume =0.0;
+	b1.setBreadth(2.3);
+	b1.setHeighth(2.0);
+	b1.setLength(2.0); 
+	volume = b1.getVolue();
+	cout << "Box1 �������" << volume <<endl;
+}
+
