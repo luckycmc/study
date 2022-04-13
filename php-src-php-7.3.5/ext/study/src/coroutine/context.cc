adb4d7034f63b96d2be8d0d27e358fc72d6fa017
zpw1765321014 <1765321014@qq.com>
2022-04-13 19:56:30 +0800

    study_continue 协成的实现

---
+++ php-src-php-7.3.5/ext/study/src/coroutine/context.cc
@@ -0,0 +1,22 @@
+#include "context.h"
+#include "study.h"
+
+using namespace Study;
+
+Context::Context(size_t stack_size, coroutine_func_t fn, void* private_data) :
+        fn_(fn), stack_size_(stack_size), private_data_(private_data)
+{
+    swap_ctx_ = nullptr;
+
+    stack_ = (char*) malloc(stack_size_);
+    
+    void* sp = (void*) ((char*) stack_ + stack_size_);
+    ctx_ = make_fcontext(sp, stack_size_, (void (*)(intptr_t))&context_func);
+}
+
+void Context::context_func(void *arg)
+{
+    Context *_this = (Context *) arg;
+    _this->fn_(_this->private_data_);
+    _this->swap_out();
+}
--- php-src-php-7.3.5/ext/study/src/coroutine/coroutine.cc
+++ php-src-php-7.3.5/ext/study/src/coroutine/coroutine.cc
@@ -4,6 +4,8 @@ using Study::Coroutine;
 
 Coroutine* Coroutine::current = nullptr;
 
+long Coroutine::last_cid = 0;
+
 void* Coroutine::get_current_task()
 {
     return Coroutine::current ? Coroutine::current->get_task() : nullptr;
@@ -12,4 +14,9 @@ void* Coroutine::get_current_task()
 void* Coroutine::get_task()
 {
     return task;
+}
+//协成的创建
+long Coroutine::create(coroutine_func_t fn, void* args = nullptr)
+{
+    return Coroutine::create(fn, args)->run();
 }
