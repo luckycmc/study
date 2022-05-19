d8674cf41223a46098f0719f097106da020761e1
admin123456 <1765321014@qq.com>
2022-05-19 22:50:49 +0800

    socket函数的实现

--- php-src-php-7.3.5/ext/study/src/coroutine/socket.cc
+++ php-src-php-7.3.5/ext/study/src/coroutine/socket.cc
@@ -2,3 +2,41 @@
 #include "socket.h"
 
 using Study::Coroutine::Socket;
+
+//socket 的创建
+Socket::Socket(int domain, int type, int protocol)
+{
+    sockfd = stSocket_create(domain, type, protocol);
+    if (sockfd < 0)
+    {
+        return;
+    }
+    stSocket_set_nonblock(sockfd);
+}
+//cocket 绑定
+int Socket::bind(int type, char *host, int port)
+{
+    return stSocket_bind(sockfd, type, host, port);
+}
+//socket 监听
+int Socket::listen()
+{
+    return stSocket_listen(sockfd);
+}
+//socket 接受客户端连接
+int Socket::accept()
+{
+    int connfd;
+
+    connfd = stSocket_accept(sockfd);
+    if (connfd < 0 && errno == EAGAIN)
+    {
+        wait_event(ST_EVENT_READ);
+        connfd = stSocket_accept(sockfd);
+    }
+
+    return connfd;
+}
+
+
+
