df25b0b9df1aa3c8858ff71fa43be9a54461da12
admin123456 <1765321014@qq.com>
2022-08-28 23:06:25 +0800

    C

--- C++/01/sptr.c
+++ C++/01/sptr.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 // 函数指针也是一种指针，是指向函数的指针
- // int(*pf) (int, int) 函数指针 类型格式相同就可以 pf就是函数指针 
+ // int(*pf) (int, int) 函数指针 类型格式相同就可以 pf就是函数指针  
+ // https://www.jb51.net/article/255237.htm 实现计算器的地址 
 void test()
 {
 	printf("haha\a");
