a27c4022bf8cd6bd228cea5cabaa14567585ccf0
admin123456 <1765321014@qq.com>
2022-09-07 22:32:19 +0800

    student 结构体传值

---
+++ C++/01/struct.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+//定义结构体
+struct  Student{
+	int age;
+	float score;
+	char name[100]
+}; 
+
+//声明函数
+
+void intput_student(struct Student *p); 
+void output_student(struct Student *p); 
+
+int main()
+{
+	return 1;
+} 
+
+void intput_student(struct Student *p){
+	
+}
+
+void output_student(struct Student *p){
+	
+}
+
