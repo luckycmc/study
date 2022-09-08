fe3c9d5d0d0741aac37aebffedbb7642fcae9fa3
admin123456 <1765321014@qq.com>
2022-09-08 22:24:27 +0800

    PHP 协成总结

--- C++/01/struct.c
+++ C++/01/struct.c
@@ -9,7 +9,7 @@ struct  Student{
 
 //声明函数
 
-void intput_student(struct Student *p); 
+void intput_student(struct Student *p);  //如果 const 修饰则 不能被改变 
 void output_student(struct Student *p); 
 
 int main()
@@ -19,13 +19,13 @@ int main()
      output_student(&p); 
 	 return 1;
 } 
-
+//输入学生的数据 
 void intput_student(struct Student *p){
 	 p->age = 30;
 	 p->score = 90.5;
 	 strcpy(p->name,"张珊");
 }
-
+ 
 void output_student(struct Student *p){
 	printf("%d %f %s\n", (*p).age, p->score, p->name);
 }
