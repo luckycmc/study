b0c9b16531dc11238e71cac66bcd49a64bef5839
admin123456 <1765321014@qq.com>
2022-09-27 23:13:42 +0800

    函数指针

--- C++/01/ptrcall2.c
+++ C++/01/ptrcall2.c
@@ -6,5 +6,31 @@ typedef struct TFT_DISPLAY
 	int y;
 	void (*p)(void); //打印函数sss 
 	int (*m)(int,int);
-}; 
+}tft_display;
 
+//打印函数
+
+void display(void)
+{
+	printf("display function\n");
+} 
+
+// 加法函数
+
+int sum(int x,int y){
+	return x+y;
+} 
+
+tft_display stu = { //结构体赋值操作 
+	.x = 1,
+	.y = 2,
+	.p = display,  //指针函数赋值 只要参数类型 返回值相同 就可以进行指向 
+	.m = sum,
+};
+//数据初始化 
+int main()
+{
+	stu.p();
+	printf("sub is %d\n",stu.m(stu.x,stu.y));
+	return 1;
+}
