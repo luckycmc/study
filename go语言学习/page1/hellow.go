cda5cc29967133ca0340f3a52981d2ce5a23f9f3
zpw <1765321014@qq.com>
2022-10-21 14:56:41 +0800

    删除 redis

--- go语言学习/page1/hellow.go
+++
@@ -1,9 +0,0 @@
-package main
-import "fmt"
-func main()  {
-	i:=1
-	for true {
-		i++
-        fmt.Println(i)
-	}
-}
--- go语言学习/page1/string.go
+++
@@ -1,7 +0,0 @@
-package main
-import "fmt"
-
-func main() {
-	var a string="我是老吴的同座"
-	fmt.Println("字符创a=",a)
-}
