49c8d390253443b07329497a54e2dc4c31f5b2bf
admin123456 <1765321014@qq.com>
2022-10-24 22:01:05 +0800

    Rpc调用

--- summary/study/swoole/代码/RPC/server.php
+++ summary/study/swoole/代码/RPC/server.php
@@ -11,15 +11,10 @@ $server->on('receive',function (\Swoole\Server $ser,$fd,$from_id,$data){
 
         //接受客户端的数据
         $data = json_decode($data,true);  //定义好相应的数据格式 进行数据通讯
-        print_r($data);
-        $returnData = [
-
-               'name'=>'zpw',
-               'age'=>'23',
-               'sex'=>1,
-        ];
+        // 获取对应的service 例如 Service/Goods
+        $reurnData = (new './service/'.$data['site'].'()')->$data['action']
         //返回数据给客户端 相当于write
-        $ser->send($fd,json_encode($returnData));
+        $ser->send($fd,$reurnData);
 });
 //启动服务器
 $server->start();
---
+++ summary/study/swoole/代码/RPC/service/Goods.php
@@ -0,0 +1,14 @@
+
+
+class Goods{
+       
+      public function index()
+      {    
+            $returnData = [
+                'name'=>'zpw',
+                'age'=>'23',
+                'sex'=>1,
+            ];
+            return json_encode($returnData);
+      }
+}
