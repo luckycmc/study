e88645e79f2100c38f5c3db624c06d8ff58eec03
admin123456 <1765321014@qq.com>
2022-11-24 10:14:10 +0800

    tcp server 修改

--- summary/study/swoole/代码/tcp/server_test_addlistener.php
+++ summary/study/swoole/代码/tcp/server_test_addlistener.php
@@ -1,7 +1,7 @@
 <?php
 $http_server = new Swoole\Http\Server('0.0.0.0',9998);
 $http_server->set(['daemonize'=> false]);
-$http_server->on('request', function ($request, $response) {
+$http_server->on('request', function (\Swoole\Http\Request $request, \Swoole\Http\Response $response) {
     $response->header("Content-Type", "text/html; charset=utf-8");
     $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
 });
@@ -11,8 +11,9 @@ $tcp_server = $http_server->listen('0.0.0.0', 9999, SWOOLE_SOCK_TCP);
 //默认新监听的端口 9999 会继承主服务器的设置，也是 HTTP 协议
 //需要调用 set 方法覆盖主服务器的设置
 $tcp_server->set([]);
-$tcp_server->on('receive', function ($server, $fd, $threadId, $data) {
-    echo $data;
+$tcp_server->on('receive', function (\Swoole\Server $server, int $fd, int $threadId, string $data) {
+    echo $data.PHP_EOL;
+    $server->send($fd,$data); //数据发送给客户端
 });
 
 $http_server->start();
