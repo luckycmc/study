a637dfe489471d3cdb759981129c1c9eafc0d815
zpw <1765321014@qq.com>
2023-05-15 22:36:12 +0800

    shop

---
+++ summary/study/swoole/tp/app/controller/Goods.php
@@ -0,0 +1,26 @@
+ <?php
+/**
+ * Created by PhpStorm.
+ * User: zpw
+ * Date: 2023/5/14
+ * Time: 14:06
+ */
+
+namespace app\controller;
+
+/**
+ * 商品排行榜
+ * Class Goods
+ * @package app\controller
+ */
+class Goods extends Base
+{
+    /**
+     * 商品排行榜
+     */
+     public function rank()
+     {
+
+     }
+
+}// class end
