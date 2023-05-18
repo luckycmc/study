2617cf6c44992a94393692a7427e3aebb33b83a4
zpw <1765321014@qq.com>
2023-05-18 19:54:13 +0800

    think-swoole

--- summary/study/swoole/tp/app/controller/v1/Goods.php
+++ summary/study/swoole/tp/app/controller/v1/Goods.php
@@ -4,6 +4,11 @@ namespace app\controller\v1;
 
 use app\controller\Base;
 
+/**
+ * v1 v2每个版本控制
+ * Class Goods
+ * @package app\controller\v1
+ */
 class Goods extends Base
 {
       public function index()
