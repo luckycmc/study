b2d5711a1ec648e6b3f4ef9f0d80e5dc44a6215b
zpw <1765321014@qq.com>
2023-08-15 23:58:42 +0800

    model数据修改

--- goland/tcp/userModel.go
+++ goland/tcp/userModel.go
@@ -4,12 +4,12 @@ type User struct {
     UserId   int    `db:"user_id"`
     Username string `db:"username"`
 }
-
+//添加数据
 func(this *User) InsertData() int{
     
 }
 
-
+//查询数据
 func(this *User) Data() int{
     
 }
