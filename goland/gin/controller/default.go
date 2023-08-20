69d58073e24612d37b44982cc93642ae583c6617
zpw <1765321014@qq.com>
2023-08-20 23:32:40 +0800

    gin换成 换成html文件

--- goland/gin/controller/default.go
+++ goland/gin/controller/default.go
@@ -10,12 +10,21 @@ type Default struct {
 
 // 前台首页
 func (this *Default) DefaultIndex(r *gin.Context) {
-
-       r.JSON(http.StatusOK, gin.H{
+       
+       //json字符串
+       /*r.JSON(http.StatusOK, gin.H{
+              "title": "zpw",
+              "ce":    "cc",
+              "score": 80,
+              "hobby": []string{"吃饭", "睡觉", "写代码"},
+       })*/
+       //字符串
+       //r.String(http.StatusOK, "index page")
+       //页面
+       r.HTML(http.StatusOK,"index.html",gin.H{
               "title": "zpw",
               "ce":    "cc",
               "score": 80,
               "hobby": []string{"吃饭", "睡觉", "写代码"},
        })
-       //r.String(http.StatusOK, "index page")
 }
