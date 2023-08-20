a180bf97df121466bdbefae9de2c8089e94b36e2
zpw <1765321014@qq.com>
2023-08-20 17:22:16 +0800

    api 动态获取参数

--- goland/gormlearn/api/user.go
+++ goland/gormlearn/api/user.go
@@ -9,9 +9,16 @@ import (
 
 //新增用户
 func SaveUser(c *gin.Context) {
-
+    //获取用户传递的id
+	idStr := c.Query("name")
+	//判断参数   
+	if len(idStr) == 0{
+		//没有携带参数是无效请求
+		c.String(200,"请求参数有问题")
+		return
+	}
 	user := &model.User{
-		Username:   "zhangsan",
+		Username:   idStr,
 		Password:   "123e4r5",
 		CreateTime: time.Now().UnixMilli(),
 	}
