d0662f27b4c11fc54cd3931d2846f49e011c42ab
zpw <1765321014@qq.com>
2023-09-01 22:11:54 +0800

    数据库处理

--- summary/study/swoole/hyperf-skeleton/config/autoload/databases.php
+++ summary/study/swoole/hyperf-skeleton/config/autoload/databases.php
@@ -18,7 +18,7 @@ return [
         'database' => env('DB_DATABASE', 'study'),
         'port' => env('DB_PORT', 3306),
         'username' => env('DB_USERNAME', 'root'),
-        'password' => env('DB_PASSWORD', 'root'),
+        'password' => env('DB_PASSWORD', '123456'),
         'charset' => env('DB_CHARSET', 'utf8'),
         'collation' => env('DB_COLLATION', 'utf8_unicode_ci'),
         'prefix' => env('DB_PREFIX', ''),
