25844877402e82f81d96546c25ac451b27b12fb9
13027679156 <1765321014@qq.com>
2023-10-28 11:37:35 +0800

    stwoole example

---
+++ example/base/http/h1.php
@@ -0,0 +1,24 @@
+<?php
+
+$http = new Swoole\Http\Server('0.0.0.0', 9501);
+
+$http->on('start', function ($server) {
+    echo "Swoole http server is started at http://127.0.0.1:9501\n";
+});
+$request = new Request();
+//绑定类中的函数  注意 绑定的函数必须是 public 属性 否则会用权限的问题
+$http->on('request', [$request,'request']);
+
+$http->start();
+
+/*function request($request, $response) {
+    $response->header('Content-Type', 'text/plain');
+    $response->end('Hello World request');
+}*/
+class Request{
+    public function request($request, $response)
+    {
+        $response->header('Content-Type', 'text/plain');
+        $response->end('Hello World request class');
+    }
+}
