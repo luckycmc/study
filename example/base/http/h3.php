d1ec33edc6cb1ba9090befe6cf1ee1c836366e05
13027679156 <1765321014@qq.com>
2023-11-04 22:56:37 +0800

    2023 11 04

--- example/base/http/h3.php
+++ example/base/http/h3.php
@@ -8,6 +8,9 @@ $http = new Swoole\Http\Server('0.0.0.0', 9501);
 $http->set([
     //'worker_num' => Swoole_cpu_num(),
     'worker_num' => 1,
+    //这个是我本机的绝对路径
+    'document_root' => '/data/C_study/example/base/http', // v4.4.0以下版本, 此处必须为绝对路径
+    'enable_static_handler' => true,
 ]);
 $http->on('start', function ($server) {
     //
@@ -18,11 +21,13 @@ $http->on('request', function(Swoole\Http\Request $request, Swoole\Http\Response
     
     //static $a[] = 'a'; //切记静态数组 不释放可能会让数组无线扩大
     //var_dump($a);
-    //设置 header 头
+    //设置 header 头 输出内容为文本
     $response->header('Content-Type', 'text/plain');
+    //输入内容为 jpeg
+    //$response->header('content-type', 'image/jpeg', true);
     //设置json的方式
     //输出数据给客户端 也就是浏览器
-    $response->end('Hello World request');
+    $response->end('index document_root');
 });
 //开启http 服务
 $http->start();
