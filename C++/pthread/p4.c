053ed2af24769891e18d4e91a4e68e77749711ef
13027679156 <1765321014@qq.com>
2024-01-02 22:28:17 +0800

    2024-01-01 用户级线程

---
+++ C++/pthread/p4.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<pthread.h>
+#include <stdlib.h>
+
+int s;
+void *func(void *arg){
+    int i ;
+    for ( i = 0; i < 10000; i++)
+    {
+        s++;
+    }  
+}
+
+int main(){
+    pthread_t th1;
+    pthread_t th2;
+    printf("main s is %d\n",s);
+    //创建两个线程
+    pthread_create(&th1,NULL,func,NULL);
+    pthread_create(&th2,NULL,func,NULL);
+
+    pthread_join(th1,NULL);
+    pthread_join(th2,NULL);
+
+    printf("s is %d\n",s);
+    return 0;
+}
