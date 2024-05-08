4b70d762e4cc95d2ac885698ab3d7cccfe82ace7
zpw <1765321014@qq.com>
2024-05-08 11:31:23 +0800

    gee框架告 一段落

--- go_study/project/test_gee/base2/main.go
+++ go_study/project/test_gee/base2/main.go
@@ -5,24 +5,31 @@ import (
 	"net/http"
 )
 
+/*
+*
+
+	主要实现 gin web的简单路由 和相应的参数的一个简单的框架
+
+*
+*/
 func main() {
 	//初始化
 	r := gee.New()
-    //第一版的数据请求
+	//第一版的数据请求
 
 	/*r.GET("/",func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
-	})
-   
-	r.GET("/hello",func(w http.ResponseWriter, r *http.Request) {
-		  for k,v := range r.Header{
-			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-		  }
-	})
-   r.GET("/login",func(w http.ResponseWriter, r *http.Request) {})*/
-   //  context 包含 res he response 的数据信息
-   //注册路由
-   /* r.GET("/",func(c *gee.Context) {
+			fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+		})
+
+		r.GET("/hello",func(w http.ResponseWriter, r *http.Request) {
+			  for k,v := range r.Header{
+				fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+			  }
+		})
+	   r.GET("/login",func(w http.ResponseWriter, r *http.Request) {})*/
+	//  context 包含 res he response 的数据信息
+	//注册路由
+	/* r.GET("/",func(c *gee.Context) {
 		   c.HTML(http.StatusOK,"<h1>Hello Gee</h1>")
 	})
 	r.GET("/hello",func(c *gee.Context) {
@@ -36,11 +43,11 @@ func main() {
 		   })
 	})*/
 	//测试带 :号的参数
-    r.GET("/hello/:name",func(ctx *gee.Context) {
-		 
-		 ctx.String(http.StatusOK,"hello %s, you're at %s\n", ctx.Param("name"), ctx.Path)
+	r.GET("/hello/:name", func(ctx *gee.Context) {
+
+		ctx.String(http.StatusOK, "hello %s, you're at %s\n", ctx.Param("name"), ctx.Path)
 	})
-    //带 *的路由
+	//带 *的路由
 	/*r.GET("/assets/*filepath", func(c *gee.Context) {
 		c.Json(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
 	})*/
