ce5dd179200c01eae70b53f5b4dace9ca30e31c7
zpw <1765321014@qq.com>
2024-05-09 10:42:01 +0800

    操作系统 文件管理的整理

--- go_study/data/digui.go
+++ go_study/data/digui.go
@@ -2,12 +2,12 @@ package main
 
 import "fmt"
 
-func Digui(n int) int{
+func Digui(n int) int {
 	//必须有结束的条件
-	 if n==0 {
-		  return 1
-	 }
-	 return n * Digui(n-1)
+	if n == 0 {
+		return 1
+	}
+	return n * Digui(n-1)
 }
 func main() {
 	fmt.Println(Digui(5))
