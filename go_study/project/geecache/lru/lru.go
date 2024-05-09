e581cd3f6ece1f6a2c2c912a4cdc2c63af3bcaed
zpw <1765321014@qq.com>
2024-05-09 19:13:05 +0800

    geecache

--- go_study/project/geecache/lru/lru.go
+++ go_study/project/geecache/lru/lru.go
@@ -19,3 +19,15 @@ type entry struct{
 type value interface{
 	Len() int
 }
+
+// New is the Constructor of Cache
+func New(maxBytes int64,OnEvicted func(string,value)) *Cache{
+
+	  return &Cache{
+		  maxBytes: maxBytes,
+		  ll: list.New(),
+		  cache: make(map[string]*list.Element),
+		  OnEvicted: OnEvicted,
+	  }
+}
+
