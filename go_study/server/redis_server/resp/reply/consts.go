0afff96fa6411b99db53c9314c06142d7cc68b42
zpw <1765321014@qq.com>
2024-05-24 17:38:19 +0800

    go-redis resp 协议解析 replay

--- go_study/server/redis_server/resp/reply/consts.go
+++ go_study/server/redis_server/resp/reply/consts.go
@@ -1,1 +1,34 @@
 package reply
+
+type PongReply struct {
+}
+
+var pongbytes = []byte("+PONG\r\n")
+
+func (r PongReply) ToBytes() []byte {
+
+	return pongbytes
+}
+
+func MakePongReply() *PongReply {
+
+	return &PongReply{}
+}
+
+/*************回复 OK start*********/
+type OkReply struct {
+}
+
+var okBytes = []byte("+OK\r\n")
+
+func (r *OkReply) ToBytes() []byte {
+	return okBytes
+}
+
+var theOkReply = new(OkReply)
+
+func MakeOkReply() *OkReply {
+	return theOkReply
+}
+
+/*************回复 OK end*********/
