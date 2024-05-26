8080514cd7ab9119e0e338bc3352acea35f14085
13027679156 <1765321014@qq.com>
2024-05-26 23:13:39 +0800

    redis-server main 代码查看

--- go_study/server/redis_server/main.go
+++ go_study/server/redis_server/main.go
@@ -9,19 +9,23 @@ import (
 	EchoHandler "redis-server/tcp"
 )
 
+// 配置文件的名称
 const configFile string = "redis.conf"
 
+// 配置文件的一些属性
 var defaultProperties = &config.ServerProperties{
 	Bind: "0.0.0.0",
 	Port: 6379,
 }
 
+// 文件是否存在
 func fileExists(filename string) bool {
 	info, err := os.Stat(filename)
 	return err == nil && !info.IsDir()
 }
 
 func main() {
+	//设置日志的配置
 	logger.Setup(&logger.Settings{
 		Path:       "logs",
 		Name:       "godis",
@@ -34,13 +38,15 @@ func main() {
 	} else {
 		config.Properties = defaultProperties
 	}
-
+	//启动tcp server 也就是 redis server
 	err := tcp.ListenAndServeWithSignal(
+		//配置项目
 		&tcp.Config{
 			Address: fmt.Sprintf("%s:%d",
 				config.Properties.Bind,
 				config.Properties.Port),
 		},
+		//处理的handler
 		EchoHandler.MakeHandler())
 	//服务器启动有误
 	if err != nil {
