7d0a675c954a7cc9775a2e00839efab1a170877d
zpw <1765321014@qq.com>
2024-05-27 10:17:10 +0800

    go-redis resp 协议的定义

--- go_study/server/redis_server/resp/reply/consts.go
+++ go_study/server/redis_server/resp/reply/consts.go
@@ -1,5 +1,23 @@
 package reply
 
+/**
+ @常用信息回复
+ ESP 定义了5种格式：
+
+简单字符串(Simple String): 服务器用来返回简单的结果，比如"OK"。非二进制安全，且不允许换行。
+错误信息(Error): 服务器用来返回简单的错误信息，比如"ERR Invalid Synatx"。非二进制安全，且不允许换行。
+整数(Integer): llen、scard 等命令的返回值, 64位有符号整数
+字符串(Bulk String): 二进制安全字符串, 比如 get 等命令的返回值
+数组(Array, 又称 Multi Bulk Strings): Bulk String 数组，客户端发送指令以及 lrange 等命令响应的格式
+
+RESP 通过第一个字符来表示格式：
+
+简单字符串：以"+" 开始， 如："+OK\r\n"
+错误：以"-" 开始，如："-ERR Invalid Synatx\r\n"
+整数：以":"开始，如：":1\r\n"
+字符串：以 $ 开始
+数组：以 * 开始
+**/
 // PongReply is +PONG
 type PongReply struct{}
 
