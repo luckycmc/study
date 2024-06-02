74b2c84172c46f7368d2f0c8f1ca488e43db3dba
13027679156 <1765321014@qq.com>
2024-06-02 21:03:47 +0800

    redis-server string 和key 实现完毕

--- go_study/server/redis_server/database/keys.go
+++ go_study/server/redis_server/database/keys.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"redis-server/interface/resp"
+	"redis-server/lib/wildcard"
 	"redis-server/resp/reply"
 )
 
@@ -18,8 +19,101 @@ func execDel(db *DB, args [][]byte) resp.Reply {
 	return reply.MakeIntReply(int64(deleted))
 }
 
+// execExists checks if a is existed in db
+func execExists(db *DB, args [][]byte) resp.Reply {
+
+	result := int64(0)
+	for _, arg := range args {
+		key := string(arg)
+		_, exists := db.GetEntity(key)
+		if exists {
+			result++
+		}
+	}
+	return reply.MakeIntReply(result)
+}
+
+// execFlushDB removes all data in current db
+func execFlushDB(db *DB, args [][]byte) resp.Reply {
+	db.Flush()
+	return &reply.OkReply{}
+}
+
+// execType returns the type of entity, including: string, list, hash, set and zset
+func execType(db *DB, args [][]byte) resp.Reply {
+	key := string(args[0])
+	entity, exists := db.GetEntity(key)
+	if !exists {
+		return reply.MakeStatusReply("none")
+	}
+	switch entity.Data.(type) {
+	case []byte:
+		return reply.MakeStatusReply("string")
+	}
+	return &reply.UnknownErrReply{}
+}
+
+// execRename a key
+func execRename(db *DB, args [][]byte) resp.Reply {
+	if len(args) != 2 {
+		return reply.MakeErrReply("ERR wrong number of arguments for 'rename' command")
+	}
+	//原来的名字
+	src := string(args[0])
+	//新名字
+	dest := string(args[1])
+
+	entity, ok := db.GetEntity(src)
+	if !ok {
+		return reply.MakeErrReply("no such key")
+	}
+	db.PutEntity(dest, entity)
+	db.Remove(src)
+	return &reply.OkReply{}
+}
+
+// execRenameNx a key, only if the new key does not exist
+func execRenameNx(db *DB, args [][]byte) resp.Reply {
+	src := string(args[0])
+	dest := string(args[1])
+
+	_, ok := db.GetEntity(dest)
+	if ok {
+		return reply.MakeIntReply(0)
+	}
+
+	entity, ok := db.GetEntity(src)
+	if !ok {
+		return reply.MakeErrReply("no such key")
+	}
+	db.Removes(src, dest) // clean src and dest with their ttl
+	db.PutEntity(dest, entity)
+	return reply.MakeIntReply(1)
+}
+
+// 匹配获取相应的名字
+// execKeys returns all keys matching the given pattern
+func execKeys(db *DB, args [][]byte) resp.Reply {
+
+	pattern := wildcard.CompilePattern(string(args[0]))
+	result := make([][]byte, 0)
+	db.data.ForEach(func(key string, val interface{}) bool {
+		if pattern.IsMatch(key) {
+			result = append(result, []byte(key))
+		}
+		return true
+	})
+	return reply.MakeMultiBulkReply(result)
+}
+
 // 初始化注册相应的命令
 func init() {
 	//命令注册
 	RegisterCommand("del", execDel, -2)
+	RegisterCommand("Exists", execExists, -2)
+	RegisterCommand("Keys", execKeys, 2)
+	RegisterCommand("FlushDB", execFlushDB, -1)
+	RegisterCommand("Type", execType, 2)
+	RegisterCommand("Rename", execRename, 3)
+	RegisterCommand("RenameNx", execRenameNx, 3)
 }
