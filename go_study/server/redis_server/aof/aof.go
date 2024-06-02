12685a0d56ae13710d94d07ec94f6171cea6e008
13027679156 <1765321014@qq.com>
2024-06-02 22:01:19 +0800

    redis-server aof的实现

--- go_study/server/redis_server/aof/aof.go
+++ go_study/server/redis_server/aof/aof.go
@@ -1,4 +1,121 @@
 package aof
 
+import (
+	"io"
+	"os"
+	"redis-server/config"
+	databaseface "redis-server/database"
+	"redis-server/lib/logger"
+	"redis-server/lib/utils"
+	"redis-server/resp/connection"
+	"redis-server/resp/parser"
+	"redis-server/resp/reply"
+	"strconv"
+)
+
+type CmdLine = [][]byte
+
+const (
+	aofQueueSize = 1 << 16
+)
+
+type payload struct {
+	cmdLine CmdLine
+	dbIndex int
+}
+
+// aof handler的描述
 type AofHandler struct {
+	db          databaseface.Database
+	aofChan     chan *payload
+	aofFile     *os.File
+	aofFilename string
+	currentDB   int
+}
+
+// 创建一个aof handler
+func NewAOFHandler(db databaseface.Database) (*AofHandler, error) {
+	handler := &AofHandler{}
+	//需要写入的文件
+	handler.aofFilename = config.Properties.AppendFilename
+	handler.db = db // db 哪个数据库
+	handler.LoadAof()
+	aofFile, err := os.OpenFile(handler.aofFilename, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0600)
+	if err != nil {
+		return nil, err
+	}
+	handler.aofFile = aofFile
+	handler.aofChan = make(chan *payload, aofQueueSize)
+	go func() {
+		handler.handleAof()
+	}()
+	return handler, nil
+}
+
+// 把数据写入到aof 中
+func (handler *AofHandler) AddAof(dbIndex int, cmdLine CmdLine) {
+	if config.Properties.AppendOnly && handler.aofChan != nil {
+		handler.aofChan <- &payload{
+			cmdLine: cmdLine,
+			dbIndex: dbIndex,
+		}
+	}
+}
+
+// 数据写入到 aof文件中去
+func (handler *AofHandler) handleAof() {
+	// serialized execution
+	handler.currentDB = 0
+	for p := range handler.aofChan {
+		if p.dbIndex != handler.currentDB {
+			// select db
+			data := reply.MakeMultiBulkReply(utils.ToCmdLine("SELECT", strconv.Itoa(p.dbIndex))).ToBytes()
+			_, err := handler.aofFile.Write(data)
+			if err != nil {
+				logger.Warn(err)
+				continue // skip this command
+			}
+			handler.currentDB = p.dbIndex
+		}
+		data := reply.MakeMultiBulkReply(p.cmdLine).ToBytes()
+		_, err := handler.aofFile.Write(data)
+		if err != nil {
+			logger.Warn(err)
+		}
+	}
+}
+
+// 记载aof  文件
+func (handler *AofHandler) LoadAof() {
+
+	file, err := os.Open(handler.aofFilename)
+	if err != nil {
+		logger.Warn(err)
+		return
+	}
+	defer file.Close()
+	ch := parser.ParseStream(file)
+	fakeConn := &connection.Connection{} // only used for save dbIndex
+	for p := range ch {
+		if p.Err != nil {
+			if p.Err == io.EOF {
+				break
+			}
+			logger.Error("parse error: " + p.Err.Error())
+			continue
+		}
+		if p.Data == nil {
+			logger.Error("empty payload")
+			continue
+		}
+		r, ok := p.Data.(*reply.MultiBulkReply)
+		if !ok {
+			logger.Error("require multi bulk reply")
+			continue
+		}
+		ret := handler.db.Exec(fakeConn, r.Args)
+		if reply.IsErrorReply(ret) {
+			logger.Error("exec err", err)
+		}
+	}
 }
