c95dbdf5a91907479aae0be5f8b8f02299d881ad
zpw <1765321014@qq.com>
2024-08-05 20:07:10 +0800

    协成池修改

--- go_study/server/goredis-main/lib/pool/pool.go
+++ go_study/server/goredis-main/lib/pool/pool.go
@@ -1,21 +1,24 @@
 package pool
 
 import (
-	"runtime/debug"
-	"strings"
-
-	"goredis-main/log"
-
 	"github.com/panjf2000/ants"
 )
 
 var pool *ants.Pool
 
-func init() {
+/*func init() {
 	_pool, err := ants.NewPool(50000, ants.WithPanicHandler(func(i interface{}) {
 		stackInfo := strings.Replace(string(debug.Stack()), "\n", "", -1)
 		log.GetDefaultLogger().Errorf("recover info: %v, stack info: %s", i, stackInfo)
 	}))
+	if err != nil {
+		panic(err)
+	}
+	pool = _pool
+}*/
+
+func init() {
+	_pool, err := ants.NewPool(50000)
 	if err != nil {
 		panic(err)
 	}
